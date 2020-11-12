e12f67e08e50ad9483d4fee90bb2fa06486aa925
Navpreet Kaur <NK028@atcltd.atc.org.nz>
2020-11-12 18:45:11 +1300

    Add project files.

---
+++ Data/ContactsApiContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactsApi.Models
+{
+    public class ContactsApiContext : DbContext
+    {
+        public ContactsApiContext (DbContextOptions<ContactsApiContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ContactsApi.Models.Contact> Contact { get; set; }
+    }
+}
---
+++ Models/Contact.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContactsApi.Models
+{
+    public class Contact // Mycontacts model class
+    {
+        [Key] // creating a primar key for db
+        public int Id { get; set; }
+        // person name
+        public string Name { get; set; }
+        // person email address
+        public string Email { get; set; }
+        // person phone number
+        public string Phone { get; set; }
+        // physical address
+        public string Address { get; set; }
+    }
+}
